9ede46009b94e16b013f7d23f9b4174a44531a5d
sudhakodali06 <72756905+sudhakodali06@users.noreply.github.com>
2020-11-08 08:18:20 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,31 @@
+class Paper{
+    constructor(x,y,r){
+        var options = {
+            isStatic: false,
+            restitution: 0.3,
+            friction: 0.5,
+            density:1.2
+        }
+
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.body = Bodies.circle(this.x,this.y,this.r,options);
+        World.add(world,this.body);
+        
+       
+    }
+
+   
+    display(){
+        var pos = this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        ellipseMode(CENTER);
+        fill("yellow");
+        ellipse(0,0,this.r,this.r);
+        pop();
+    }
+
+    
+}
